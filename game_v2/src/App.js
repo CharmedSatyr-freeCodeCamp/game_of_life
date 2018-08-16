d35fb3e9b770400133cc668c7857f6412d838fd9
CharmedSatyr <z@charmed.tech>
2018-08-16 18:26:12 -0500

    basic toggleable cells; need to convert to redux

--- game_v2/src/App.js
+++ game_v2/src/App.js
@@ -1,22 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class Cell extends Component {}
+const hw = 20; // Cell height and width
+class Cell extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { alive: false };
+    this.toggleAlive = this.toggleAlive.bind(this);
+  }
+  toggleAlive() {
+    this.setState({ alive: this.state.alive ? false : true });
+  }
+  render() {
+    return (
+      <div
+        onClick={this.toggleAlive}
+        style={{
+          backgroundColor: this.state.alive ? 'gold' : 'lightblue',
+          boxShadow: '0 0 1px black',
+          width: hw,
+          fontSize: 4,
+          height: hw,
+        }}
+      >
+        {this.props.idx}
+      </div>
+    );
+  }
+}
+
+const makeCells = num => {
+  const cells = [];
+  for (let i = 0; i < num; i++) {
+    cells.push(<Cell key={i} idx={i} alive={false} />);
+  }
+  return cells;
+};
+
+class Grid extends Component {
+  componentDidMount() {}
+  render() {
+    return (
+      <div style={{ boxShadow: '0 0 1px black', display: 'flex', flexWrap: 'wrap', width: 500 }}>
+        {makeCells(400)}
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <Grid />;
   }
 }
 
