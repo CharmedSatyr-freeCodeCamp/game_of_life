2c331227f52a1aac0ccf86d216406e4c50edac75
CharmedSatyr <z@charmed.tech>
2018-09-23 11:09:46 -0500

    +scss object

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import App from './components/App';
 import store from './store';
 import registerServiceWorker from './registerServiceWorker';
 
+import styles from './styles/styles.scss';
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
