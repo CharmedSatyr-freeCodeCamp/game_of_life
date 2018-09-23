a7e39e7a5f22e988b2f017bc43cc52cd2523c232
CharmedSatyr <z@charmed.tech>
2018-09-23 10:59:11 -0500

    +header/footer

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    <h5>
+      <a
+        href="https://github.com/CharmedSatyr/game_of_life"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Source code
+      </a>{' '}
+      available under the{' '}
+      <a
+        href="https://github.com/CharmedSatyr/game_of_life/blob/master/LICENSE"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        GNU Public License v3.0
+      </a>
+    </h5>
+  </footer>
+);
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <h1>John Conway's Game of Life</h1>
+    <small>a zero player automaton</small>
+  </header>
+);
+
+export default Header;
