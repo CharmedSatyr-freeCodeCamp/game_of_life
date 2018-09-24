64a2036ac05bb635e7de1db4787eb07019085a3a
CharmedSatyr <z@charmed.tech>
2018-09-24 17:48:52 -0500

    reduce gridsize and increase cell dimensions

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,6 @@
 // Constants
-export const gridHeight = 50;
-export const gridWidth = 80;
-export const cellDimensions = 10; // Cell height and width in pixels
+export const gridHeight = 20;
+export const gridWidth = 30;
+export const cellDimensions = 20; // Cell height and width in pixels
 export const probability = 0.8; // Default probability a cell is alive on randomize
 export const duration = 100; // Duration in ms of each generation
