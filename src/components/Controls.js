408f71c4ee5b2b971ef902958399272336640fc0
CharmedSatyr <z@charmed.tech>
2018-09-26 08:49:54 -0500

    updated button names, basic selection styling

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -5,50 +5,75 @@ import * as c from '../constants/constants';
 // Font Awesome
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPause, faPlay, faRandom, faTimes } from '@fortawesome/free-solid-svg-icons';
-library.add(faPause, faPlay, faRandom, faTimes);
+import {
+  faPause,
+  faPlay,
+  faRandom,
+  faStepForward,
+  faTimes,
+} from '@fortawesome/free-solid-svg-icons';
+library.add(faPause, faPlay, faRandom, faStepForward, faTimes);
 
 export default class Controls extends Component {
   constructor(props) {
     super(props);
     this.clear = this.clear.bind(this);
-    this.makeGrid = this.makeGrid.bind(this);
-    this.start = this.start.bind(this);
-    this.stop = this.stop.bind(this);
+    this.random = this.random.bind(this);
+    this.play = this.play.bind(this);
+    this.pause = this.pause.bind(this);
+    this.step = this.step.bind(this);
+
+    this.state = { selected: null };
   }
   clear() {
     clearInterval(this.duration);
     this.props.clear();
+    this.setState({ selected: 'clear' });
   }
-  makeGrid() {
+  pause() {
     clearInterval(this.duration);
-    this.props.makeGrid();
+    this.setState({ selected: 'pause' });
   }
-  start() {
+  play() {
     clearInterval(this.duration);
     this.duration = setInterval(() => {
       this.props.nextGen();
     }, c.duration);
+    this.setState({ selected: 'play' });
   }
-  stop() {
+  random() {
     clearInterval(this.duration);
+    this.props.makeGrid();
+    this.setState({ selected: 'random' });
+  }
+  step() {
+    this.props.nextGen();
   }
   render() {
+    const { selected } = this.state;
     return (
       <div className="controls">
-        <button onClick={this.start}>
+        <button onClick={this.step}>
+          <FontAwesomeIcon icon="step-forward" />
+          &nbsp;Step
+        </button>
+
+        <button className={selected === 'play' && 'selected'} onClick={this.play}>
           <FontAwesomeIcon icon="play" />
           &nbsp;Start
         </button>
-        <button onClick={this.stop}>
+
+        <button className={selected === 'pause' && 'selected'} onClick={this.pause}>
           <FontAwesomeIcon icon="pause" />
           &nbsp;Pause
         </button>
-        <button onClick={this.clear}>
+
+        <button className={selected === 'clear' && 'selected'} onClick={this.clear}>
           <FontAwesomeIcon icon="times" />
           &nbsp;Clear
         </button>
-        <button onClick={this.makeGrid}>
+
+        <button className={selected === 'random' && 'selected'} onClick={this.random}>
           <FontAwesomeIcon icon="random" />
           &nbsp;Random
         </button>
