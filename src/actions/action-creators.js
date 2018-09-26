340a46f5c5d827a757beb102ccab3dccf24ec7d6
CharmedSatyr <z@charmed.tech>
2018-09-26 14:11:48 -0500

    simplify syntax

--- src/actions/action-creators.js
+++ src/actions/action-creators.js
@@ -1,27 +1,7 @@
 import * as a from '../constants/action-types';
 
 // Action Creators
-export const clear = () => {
-  return {
-    type: a.CLEAR,
-  };
-};
-
-export const makeGrid = () => {
-  return {
-    type: a.MAKE_GRID,
-  };
-};
-
-export const nextGen = () => {
-  return {
-    type: a.NEXT_GEN,
-  };
-};
-
-export const toggle = index => {
-  return {
-    index,
-    type: a.TOGGLE,
-  };
-};
+export const clear = () => ({ type: a.CLEAR });
+export const makeGrid = () => ({ type: a.MAKE_GRID });
+export const nextGen = () => ({ type: a.NEXT_GEN });
+export const toggle = index => ({ index, type: a.TOGGLE });
