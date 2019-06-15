eb65e0a30840b7ed4df258cff532d5e7a8bb2008
Joseph <z@charmed.tech>
2019-06-15 11:48:25 -0700

    Simplify method binding with arrow function

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,13 +3,9 @@ import PropTypes from 'prop-types';
 import * as c from '../constants/constants';
 
 export default class Cell extends Component {
-  constructor(props) {
-    super(props);
-    this.toggle = this.toggle.bind(this);
-  }
-  toggle() {
+  toggle = () => {
     this.props.toggle(this.props.index);
-  }
+  };
   render() {
     return (
       <div
