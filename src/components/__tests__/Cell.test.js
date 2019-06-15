189e722a2fd5ca0230d39cb7b00cd34cfcccdae4
Joseph <z@charmed.tech>
2019-06-15 11:48:09 -0700

    Add 100% coverage for Cell component

--- src/components/__tests__/Cell.test.js
+++ src/components/__tests__/Cell.test.js
@@ -9,9 +9,37 @@ const { boolean, number } = random;
 configure({ adapter: new Adapter() });
 
 describe('<Cell /> component', () => {
-  const props = { alive: boolean(), index: number(), toggle: jest.fn() };
+  const props = { index: number(), toggle: jest.fn() };
+
   it('renders without crashing', () => {
+    props.alive = boolean();
     const cell = shallow(<Cell {...props} />);
     expect(cell.exists()).toBeTruthy();
+  });
+
+  it('should have class `cell alive` if its `alive` prop is `true`', () => {
+    props.alive = true;
+    const cell = shallow(<Cell {...props} />);
+    expect(cell.find('.cell').exists()).toBeTruthy();
+    expect(cell.find('.alive').exists()).toBeTruthy();
+    expect(cell.find('.dead').exists()).toBeFalsy();
+  });
+
+  it('should have class `cell dead` if its `alive` prop is `false`', () => {
+    props.alive = false;
+    const cell = shallow(<Cell {...props} />);
+    expect(cell.find('.cell').exists()).toBeTruthy();
+    expect(cell.find('.dead').exists()).toBeTruthy();
+    expect(cell.find('.alive').exists()).toBeFalsy();
+  });
+
+  describe('`toggle` method', () => {
+    props.alive = boolean();
+    const cell = shallow(<Cell {...props} />);
+
+    it('should call `props.toggle` with `props.index` on click', () => {
+      cell.simulate('click');
+      expect(props.toggle).toHaveBeenCalledWith(props.index);
+    });
   });
 });
