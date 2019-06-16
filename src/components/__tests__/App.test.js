6dcb49a2cbcad36ccfa6bde2cc8abcaf7b95cf89
Joseph <z@charmed.tech>
2019-06-15 18:16:15 -0700

    Update <App /> tests to check contained components

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import configureMockStore from 'redux-mock-store';
 
 import App from '../App';
 import Header from '../Header';
@@ -10,16 +9,8 @@ import Footer from '../Footer';
 
 configure({ adapter: new Adapter() });
 
-const initialState = {
-  generation: 0,
-  cellData: [],
-};
-
-const mockStore = configureMockStore();
-let store;
 let app;
 beforeAll(() => {
-  store = mockStore(initialState);
   app = shallow(<App />);
 });
 
@@ -28,18 +19,15 @@ describe('<App />', () => {
     expect(app.exists()).toBeTruthy();
   });
 
-  xit('contains an instance of <Header />', () => {
-    const header = shallow(<Header />);
-    expect(app.find(header)).toHaveLength(1);
+  it('contains an instance of <Header />', () => {
+    expect(app.find(Header)).toHaveLength(1);
   });
 
-  xit('contains an instance of <Game />', () => {
-    const game = shallow(<Game store={store} />);
-    expect(app.find(game)).toHaveLength(1);
+  it('contains an instance of <Game />', () => {
+    expect(app.find(Game)).toHaveLength(1);
   });
 
-  xit('contains an instance of <Footer />', () => {
-    const footer = shallow(<Footer />);
-    expect(app.find(footer)).toHaveLength(1);
+  it('contains an instance of <Footer />', () => {
+    expect(app.find(Footer)).toHaveLength(1);
   });
 });
